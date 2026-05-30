a07e892fe3e5b167ef5a53c9cefe72004693f0b6
Dora Yeh <dorayeh1313@gmail.com>
2026-05-30 16:17:49 +0800

    add: supabase url and publickey

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Get these from your Supabase Project Settings -> API
 // These are safe to share publicly in a client-side app
 window.SUPABASE_CONFIG = {
-  url: "https://your-project-id.supabase.co",
-  anonKey: "your-anon-public-key"
+  url: "https://zueqikwdqozlirrhlatg.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1ZXFpa3dkcW96bGlycmhsYXRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMjA2MzgsImV4cCI6MjA5NTY5NjYzOH0.pWIrTFmq13c_14qauL09kaP3c_USEyTRvjxew9EWU0w"
 };
